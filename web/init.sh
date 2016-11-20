32524f9a50f9c96944cf3085eaa2b9dfd35ce862
Timofey Asyrkin <timofeya@amazon.com>
2016-11-19 18:02:54 -0800

    Removing ngnix's default server config

--- web/init.sh
+++ web/init.sh
@@ -1,4 +1,5 @@
-sudo ln -s /home/box/web/etc/nginx.conf /etc/nginx/sites-enabled/tasyrkin_web.conf
+sudo rm /etc/ngnix/sites-enabled/default
+sudo ln -sf /home/box/web/etc/nginx.conf /etc/nginx/sites-enabled/tasyrkin_web.conf
 sudo /etc/init.d/nginx restart
 # sudo ln -s /home/box/web/etc/gunicorn.conf /etc/gunicorn.d/test
 # sudo /etc/init.d/gunicorn restart
