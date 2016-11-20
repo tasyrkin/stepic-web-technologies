ad0ea410bd5cf67002c369aa34fd215d3743f376
Timofey Asyrkin <timofeya@amazon.com>
2016-11-19 22:50:51 -0800

    More logging to web/init.bash script

--- web/init.sh
+++ web/init.sh
@@ -1,7 +1,20 @@
 #!/bin/bash
 
+PRJ_NAME=stepic-web-technologies
+
+echo "mv $HOME/$PRJ_NAME/web ."
+mv $HOME/$PRJ_NAME/web .
+
+echo "rm -rf $HOME/$PRJ_NAME"
+rm -rf $HOME/$PRJ_NAME
+
+echo "sudo rm /etc/nginx/sites-enabled/default"
 sudo rm /etc/nginx/sites-enabled/default
+
+echo "sudo ln -sf /home/box/web/etc/nginx.conf /etc/nginx/sites-enabled/tasyrkin_web.conf"
 sudo ln -sf /home/box/web/etc/nginx.conf /etc/nginx/sites-enabled/tasyrkin_web.conf
+
+echo "sudo /etc/init.d/nginx restart"
 sudo /etc/init.d/nginx restart
 # sudo ln -s /home/box/web/etc/gunicorn.conf /etc/gunicorn.d/test
 # sudo /etc/init.d/gunicorn restart
