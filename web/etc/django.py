cbbd51a0ee1689b944d1e5fd59d87e7cbb67e17b
Timofey Asyrkin <timofeya@amazon.com>
2016-11-23 09:38:26 -0800

    Switching 127.0.0.1 -> 0.0.0.0

--- web/etc/django.py
+++ web/etc/django.py
@@ -15,7 +15,7 @@ CONFIG = {
     # 'user': 'www-data',
     # 'group': 'www-data',
     'args': (
-        '--bind=127.0.0.1:8000',
+        '--bind=0.0.0.0:8000',
         # '--workers=4',
         # '--worker-class=egg:gunicorn#sync',
         '--timeout=60',
